67dba6439f510de1c9d34994b766bd0538ddb1ed
amrufathy <amru_fathy@yahoo.com>
2016-08-11 01:27:54 +0200

    fix categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160809205114) do
+ActiveRecord::Schema.define(version: 20160809215958) do
 
   create_table "categories", force: :cascade do |t|
     t.integer  "parent_id",          limit: 4
@@ -28,11 +28,11 @@ ActiveRecord::Schema.define(version: 20160809205114) do
   create_table "products", force: :cascade do |t|
     t.integer  "category_id",        limit: 4
     t.string   "name",               limit: 255
-    t.decimal "price_pharmacy", precision: 10, scale: 6
-    t.decimal "price_population", precision: 10, scale: 6
+    t.decimal  "price_pharmacy",                   precision: 10, scale: 6
+    t.decimal  "price_population",                 precision: 10, scale: 6
     t.text     "comments",           limit: 65535
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                                null: false
+    t.datetime "updated_at",                                                null: false
     t.string   "photo_file_name",    limit: 255
     t.string   "photo_content_type", limit: 255
     t.integer  "photo_file_size",    limit: 4

1f0ca6242a614c6d1858e3099e7bb9805d649bc0
memmit1994 <memmit1994@gmail.com>
2016-08-12 04:12:18 +0200

    added after sign-in redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
+  protect_from_forgery with: :exceptiongit
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    # if user.position == 'admin' or user.position == 'board'
-    #   events_path
-    # else
-    #   users_volunteer_dashboard_path
-    # end
+    if user.position == 'admin'
+      admin_dashboard_path
+    else
+      client_dashboard_path
+    end
   end
 
   protected

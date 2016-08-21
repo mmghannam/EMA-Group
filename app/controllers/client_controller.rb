2d556c2598ac45545b1f1067e08a683d11d2a023
memmit1994 <memmit1994@gmail.com>
2016-08-21 22:42:28 +0200

    new arrivals in homepage

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -2,6 +2,6 @@ class ClientController < ApplicationController
   def default
     @parent_categories = Category.where(parent_id: nil)
     # @featured_products =
-    @new_arrivals = Product.order(created_at: :desc).limit(10).map(&:name)
+    @new_arrivals = Product.order(created_at: :desc).limit(10)
   end
 end

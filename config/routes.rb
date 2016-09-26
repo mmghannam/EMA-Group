65bf92790c4ae01bbebbef1487e76dcafce11159
memmit1994 <memmit1994@gmail.com>
2016-09-26 22:36:43 +0200

    Fixed error in order forms in client->products page

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
     # resources :categories
     # resources :products
     # resources :carts
-    # resources :orders
+    resources :orders
     resources :offers, only: [:index]
 
     # The priority is based upon order of creation: first created -> highest priority.
